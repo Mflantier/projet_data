e1888f4a7d4e56ba70d7a83a463986b2c21261f6
vagrant <vagrant@vagrant.vm>
2019-01-16 10:46:08 +0000

    Affichage liste pays : nom + alphacode

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,4 +11,26 @@ require('../scss/app.scss');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
 
-console.log('Hello fide !!!!');
+
+const pays = document.querySelector('#pays');
+
+fetch(`https://restcountries.eu/rest/v2/all`)
+    .then((res) => {
+        return res.json();
+    })
+    .then((res) => {
+        console.log(res);
+
+            for (i = 0; i < 250; i++) {
+                let li = document.createElement("li");
+                li.innerHTML = "<b>Nom : </b>"+res[i]['translations']['fr'];
+                li.classList.add("liste-pays");
+                li.id = res[i]['alpha3Code'].toLowerCase();
+                pays.appendChild(li);
+            };
+    })
+    .catch((err) => {
+        if (err) {
+            console.log(err);
+        };
+    })

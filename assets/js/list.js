9d64278ce687d31755705d7267fa824c0de87295
ibrahimsow <ibrahim.s@codeur.online>
2019-01-22 11:05:00 +0000

    design & responsive list-pays working

--- assets/js/list.js
+++ assets/js/list.js
@@ -10,7 +10,7 @@ fetch(`https://restcountries.eu/rest/v2/all`)
         for (i = 0; i < 250; i++) {
             let li = document.createElement("p");
             li.innerHTML = res[i]['translations']['fr'];
-            li.classList.add("liste-pays","text-center");
+            li.classList.add("liste-pays","text-center","col-12","col-sm-6","col-md-3","col-lg-3","col-xl-3");
             li.id = res[i]['alpha3Code'].toLowerCase();
             pays.appendChild(li);
         };

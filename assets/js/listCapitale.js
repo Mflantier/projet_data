faa196638690ae8e02f7ae69e0ff664ff903533f
vagrant <vagrant@vagrant.vm>
2019-01-22 11:12:43 +0000

    lightbox liste capitale

--- assets/js/listCapitale.js
+++ assets/js/listCapitale.js
@@ -1,22 +1,96 @@
 // Affichage liste de toutes les capitales
 const pays = document.querySelector('#pays');
 
-fetch(`https://restcountries.eu/rest/v2/all`)
-    .then((res) => {
-        return res.json();
-    })
-    .then((res) => {
-        for (i = 0; i < 250; i++) {
-            let p = document.createElement("p");
-            p.innerHTML = "<b>Pays : </b>" + res[i]['translations']['fr'] + " <b>Capitale : </b>" + res[i]['capital'];
-            p.classList.add("liste-capitale");
-            p.classList.add("col-3");
-            p.id = res[i]['alpha3Code'].toLowerCase();
-            pays.appendChild(p);
-        };
-    })
-    .catch((err) => {
-        if (err) {
-            console.log(err);
-        };
-    })
+function leFetch(callBack) {
+    fetch(`https://restcountries.eu/rest/v2/all`)
+        .then((res) => {
+            return res.json();
+        })
+        .then((res) => {
+            for (i = 0; i < 250; i++) {
+                let p = document.createElement("p");
+                p.innerHTML = "<b>Pays : </b>" + res[i]['translations']['fr'] + " <b>Capitale : </b>" + res[i]['capital'];
+                p.classList.add("liste-capitale");
+                p.setAttribute('data-toggle', 'modal');
+                p.setAttribute('data-target', '#choix');
+                p.classList.add("col-3");
+                p.id = res[i]['alpha3Code'].toLowerCase();
+                pays.appendChild(p);
+            };
+        })
+        .catch((err) => {
+            if (err) {
+                console.log(err);
+            };
+        });
+    setTimeout(function () {
+        callBack();
+    }, 1000);
+
+}
+
+function affichage() {
+    const nom = document.querySelector('.modal-title');
+    const choice = document.querySelector('.modal-body');
+    const node = document.querySelectorAll('.liste-capitale');
+    for (i = 0; i < node.length; i++) {
+        node[i].addEventListener('click', function (e) {
+            let aCode = e.target.id;
+
+            fetch(`https://restcountries.eu/rest/v2/alpha/` + aCode)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((response) => {
+                    while (choice.firstChild) {
+                        choice.removeChild(choice.firstChild);
+                    }
+                    let drapeau = document.createElement("p");
+                    let continent = document.createElement("p");
+                    let capitale = document.createElement("p");
+                    let population = document.createElement("p");
+                    let superficie = document.createElement("p");
+                    let langue = document.createElement("p");
+                    let devise = document.createElement("p");
+                    let voisins = document.createElement("ul");
+
+                    nom.textContent = response['translations']['fr'];
+                    drapeau.innerHTML = '<img src="' + response['flag'] + '" width="150" height="100">';
+                    continent.innerHTML = 'Continent : ' + response['region'];
+                    capitale.innerHTML = 'Capitale : ' + response['capital'];
+                    population.innerHTML = 'Population : ' + response['population'];
+                    superficie.innerHTML = 'Superficie : ' + response['area'];
+                    langue.innerHTML = 'Langue : ' + response['languages'][0]['nativeName'];
+                    devise.innerHTML = 'Devise : ' + response['currencies'][0]['name'];
+                    voisins.innerHTML = 'Pays voisins : ';
+
+                    for (i = 0; i < response['borders'].length; i++) {
+                        fetch(`https://restcountries.eu/rest/v2/alpha/` + response['borders'][i])
+                            .then((response) => {
+                                return response.json();
+                            })
+                            .then((response) => {
+                                let voisin = document.createElement("li");
+                                voisin.innerHTML = response['translations']['fr'];
+                                voisins.appendChild(voisin);
+                            })
+                    }
+                    choice.appendChild(drapeau);
+                    choice.appendChild(continent);
+                    choice.appendChild(capitale);
+                    choice.appendChild(population);
+                    choice.appendChild(superficie);
+                    choice.appendChild(langue);
+                    choice.appendChild(devise);
+                    choice.appendChild(voisins);
+                })
+                .catch((err) => {
+                    if (err) {
+                        console.log(err);
+                    };
+                })
+        })
+    }
+}
+
+leFetch(affichage);

8ed263f32eb3a647a5d94702b76a72ded8b936cd
ibrahimsow <ibrahim.s@codeur.online>
2019-01-22 13:54:45 +0000

    responsive ok

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,11 +1,11 @@
 const monmenu = document.querySelector(".showmenu");
-const monhamberger = document.querySelector(".hamburger");
+const monhamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".closeBtn");
 const btnContinent = document.querySelector(".btn-continent");
 const sousMenuContinents = document.querySelector(".sous-menu-continents");
 
 
-monhamberger.addEventListener("click", function(showmenu){
+monhamburger.addEventListener("click", function(showmenu){
        document.querySelector(".showmenu").style.left = "0vw";
        document.querySelector(".hamburger").style.visibility = "hidden";
  })

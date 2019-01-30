ab1defcb6c3c3598ddc65f0d5050d037832b3f3f
vagrant <vagrant@vagrant.vm>
2019-01-30 10:29:56 +0000

    Colonnes lightbox

--- assets/js/infoLightBox.js
+++ assets/js/infoLightBox.js
@@ -35,7 +35,6 @@ export default function affichageLightBox(tab) {
                     let langue = document.createElement("p");
                     let devise = document.createElement("p");
                     let voisins = document.createElement("ul");
-                    voisins.classList.add("p-0");
 
                     nom.textContent = tab[p][0];
                     drapeau.setAttribute("src", tab[p][2]);
